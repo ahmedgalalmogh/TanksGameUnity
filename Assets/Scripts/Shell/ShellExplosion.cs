4c4d284e753b3a1c3f419f1ac416dbeb5ceab035
ahmedgalalmogha <ahmed.galal.fcis@gmail.com>
2021-10-12 14:34:19 +0200

    intial Commit

---
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+        for (int i=0;i<colliders.Length;i++)
+        {
+            Rigidbody rigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!rigidbody)
+                continue;
+            rigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+            TankHealth targetHealth = rigidbody.GetComponent<TankHealth>();
+            if (!targetHealth)
+                continue;
+            float damage = CalculateDamage(rigidbody.position);
+            targetHealth.TakeDamage(damage);
+        }
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+        Destroy(gameObject);
+        // Find all the tanks in an area around the shell and damage them.
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+        float damage = relativeDistance * m_MaxDamage;
+        damage = Mathf.Max(0f, damage);
+        return damage;
+    }
+}
